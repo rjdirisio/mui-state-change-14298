e12c6a713a231a881838e11fe5a1b2492ba742f4
Ryan DiRisio <rdirisio@treeline.bio>
2024-08-23 12:01:13 -0400

    remove commented out lines in vite config

--- vite.config.js
+++ vite.config.js
@@ -8,26 +8,8 @@ export default defineConfig(() => {
     define: {
       "process.env": process.env,
     },
-    // plugins: [
-    //   react({
-    //     jsxImportSource: "@emotion/react",
-    //     babel: {
-    //       plugins: ["@emotion/babel-plugin"],
-    //     },
-    //   }),
-    // ],
     server: {
       open: true,
-    },
-    // https://github.com/mui/material-ui/issues/32727#issuecomment-1967192208
-    optimizeDeps: {
-      include: [
-        // "@mui/material/Tooltip",
-        // "@emotion/styled",
-        // "@emotion/react",
-        // "@mui/material/Unstable_Grid2",
-        // "@mui/icons-material",
-      ],
     },
   };
 });
